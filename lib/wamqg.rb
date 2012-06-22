3ce07f4371cf8146031c9a13778624793d8bbe20
franzk <franzk@xyz>
2012-06-22 14:23:56 +0200

    first working code

--- lib/wamqg.rb
+++ lib/wamqg.rb
@@ -1,8 +1,32 @@
+require 'json'
+
 class WAMQG
   class << self
 
     def start
+      EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
+        ws.onopen do
+          puts "WebSocket connection open: #{ws.inspect}"
+        end
 
+        ws.onclose do
+          puts "Connection closed"
+        end
+
+        ws.onmessage { |message|
+          puts "Received message: #{message}"
+          if message =~ /^bind (.*)/
+            routing_key = $1
+            CHANNEL.queue('wamqg', auto_delete: true).bind(EXCHANGE, routing_key: routing_key).subscribe do |headers,payload|
+              _message = {
+                routing_key: headers.routing_key,
+                payload: payload
+              }
+              ws.send _message.to_json
+            end
+          end
+        }
+      end
     end
 
     def stop
